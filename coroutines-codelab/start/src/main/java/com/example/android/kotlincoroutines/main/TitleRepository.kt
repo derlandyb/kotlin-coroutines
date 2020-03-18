fb4aa7120745569075766a98e304979b212bb7e4
Derlandy Belchior <d.belchior82@gmail.com>
2020-03-18 18:12:37 -0300

    Step 8 - Making main-safe functions from blocking code

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,7 +19,9 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -43,9 +45,22 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
     val title: LiveData<String?> = titleDao.titleLiveData.map { it?.title }
 
 
-    // TODO: Add coroutines-based `fun refreshTitle` here
+
     suspend fun refreshTitle() {
-        delay(500)
+        withContext(Dispatchers.IO) {
+            val result = try {
+                // Making networking request using a block call
+                network.fetchNextTitle().execute()
+            } catch (cause: Throwable) {
+                throw TitleRefreshError("Unable to refresh title", cause)
+            }
+
+            if(!result.isSuccessful) {
+                throw TitleRefreshError("Unable to refresh title", null)
+            }
+
+            titleDao.insertTitle(Title(result.body()!!))
+        }
     }
 
     /**
